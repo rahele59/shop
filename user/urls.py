77de6858e6b6011e43604724cafda8b1fabfe8f5
rahele59 <shafieerahele23@gmail.com>
2025-01-01 14:55:17 +0330

    create update pass create update profile finish user api

--- user/urls.py
+++ user/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('register', Register.as_view(), name='register'),
     path('login', Login.as_view(), name='login'),
     path('forget_pass', ForgetPass.as_view(), name='forget_pass'),
-    path('update_pass', UpdatePass.as_view(), name='forget_pass'),
+    path('update_pass', UpdatePass.as_view(), name='update_pass'),
     path('update_profile', UpdateProfile.as_view(), name='update_profile'),
 ]
